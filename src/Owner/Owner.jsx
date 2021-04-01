44f7d14673a41cc01215db72650a0613b72d3aa9
Fowsia Elmi <fowsiaelmi@Fowsias-MBP.zyxel.com>
2021-04-01 14:10:15 -0500

    we are getting back data fetching all owners

--- src/Owner/Owner.jsx
+++ src/Owner/Owner.jsx
@@ -1,22 +1,27 @@
 /* Import Libraries */
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function Owner() {
-  const dispatch = useDispatch();
-  const [newOwner, setNewOwner] = useState('');
+  useEffect(() => {
+    dispatch({ type: "FETCH_OWNERS" });
+  }, []);
 
+  const dispatch = useDispatch();
+  const [newOwner, setNewOwner] = useState("");
+  const owners = useSelector((store)=>store.owners)
+  console.log('owners is', owners)
+  console.log('owners at ', owners[0])
   const addOwner = (event) => {
     event.preventDefault();
-    console.log('*** <Owner /> -> addOwner() ***');
-    console.log('newOwner:', newOwner);
-
+    console.log("*** <Owner /> -> addOwner() ***");
+    console.log("newOwner:", newOwner);
     dispatch({
       type: "CREATE_OWNER",
       payload: {
-        name: newOwner
-      }
-    })
+        name: newOwner,
+      },
+    });
   };
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,48 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App/App';
-import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
-import { takeEvery, put } from 'redux-saga/effects'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App/App";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { takeEvery, put } from "redux-saga/effects";
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
-// const reducerName = (state = 0, action) => 
-// {return state;}` for each reducer (variable) needed -> 
+// const reducerName = (state = 0, action) =>
+// {return state;}` for each reducer (variable) needed ->
 // listed before next line'
+
+const owners = (state = [], action) => {
+  switch (action.type) {
+    case "SET_OWNERS":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 function* rootSaga() {
   yield takeEvery("CREATE_OWNER", addNewOwner);
-  // yield takeEvery("FETCH_OWNERS", fetchOwners);
+  yield takeEvery("FETCH_OWNERS", fetchOwners);
 }
 
 function* addNewOwner(action) {
-  console.log('addNewOwner action.payload:', action.payload);
+  console.log("addNewOwner action.payload:", action.payload);
 
   const newOwnerName = action.payload;
 
   try {
-    yield axios.post('/owners', newOwnerName)
+    yield axios.post("/owners", newOwnerName);
 
     // yield put({
     //   type: "FETCH_OWNERS"
     // })
+  } catch (error) {
+    console.log("ERROR posting new owner:", error);
   }
-  catch (error) {
-    console.log('ERROR posting new owner:', error);
+}
 
+function* fetchOwners(action) {
+  try {
+    const owners = yield axios.get("/owners");
+    console.log("got a response on fetch owners", owners.data);
+    yield put({ type: "SET_OWNERS", payload: owners.data });
+  } catch (err) {
+    console.log("error on fetch owners", err);
   }
 }
 
 const store = createStore(
   combineReducers({
-    addNewOwner,
-  }), applyMiddleware(logger, sagaMiddleware));
+    owners,
+  }),
+  applyMiddleware(logger, sagaMiddleware)
+);
 
 sagaMiddleware.run(rootSaga);
 
@@ -52,7 +71,7 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
